101bd73390b0a3f0392d966e613c8430215ad2b9
olgun-yilmaz <olgunyilm@gmail.com>
2025-02-16 19:37:49 +0300

    Added JokeViewModel

--- app/src/main/java/com/olgunyilmaz/programmingjokesapp/service/JokeAPI.kt
+++ app/src/main/java/com/olgunyilmaz/programmingjokesapp/service/JokeAPI.kt
@@ -2,11 +2,10 @@ package com.olgunyilmaz.programmingjokesapp.service
 
 import retrofit2.http.GET
 import com.olgunyilmaz.programmingjokesapp.model.Joke
-import retrofit2.Call
 
 interface JokeAPI {
 
     @GET("atilsamancioglu/ProgrammingJokesJSON/refs/heads/main/jokes.json")
-    fun fetchData(): Call<List<Joke>>
+    fun fetchData(): List<Joke>
 
 }
